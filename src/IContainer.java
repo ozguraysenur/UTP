2b96d46cfdff3e40ffbf3a88d65f63c544b1f8a4
ozguraysenur <54063783+ozguraysenur@users.noreply.github.com>
2019-11-18 21:37:07 +0100

    Delete IContainer.java

--- src/IContainer.java
+++
@@ -1,8 +0,0 @@
-
-public interface IContainer<TElement extends IAggregable<TElement, TResult> & IDeeplyCloneable<TElement>, TResult>
-{
-
-    TResult aggregateAllElements();
-
-    TElement cloneElementAtIndex(int index);
-}
